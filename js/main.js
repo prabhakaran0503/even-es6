0ecb9114cf4427d48350982b9769adfdf92e9d8c
prabhakaran <prabhakaranplkk@gmail.com>
2023-10-25 09:36:06 +0530

    even

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+let n=prompt("enter the value");
+
+let fun=(n)=>{
+    if(n%2==0){
+        document.write(`The entered number is  even`)
+    }
+    else{
+        document.write(`the entered number is odd`)
+    }
+}
+fun(n)
